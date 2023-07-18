ed30cadc56fefdd341671eeb11fe891f4974542f
Theo <theo@iconara.net>
2023-07-18 13:06:48 +0200

    Highlight the selected item in the menu

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -1,7 +1,19 @@
 import {component$} from '@builder.io/qwik'
-import {Link} from '@builder.io/qwik-city'
+import {Link, useLocation} from '@builder.io/qwik-city'
 import styles from './header.module.css'
 import BetongromantikLogo from '~/media/betongromantik.svg?jsx'
+
+const NavLink = component$<{href: string, title: string}>(({href, title}) => {
+  const location = useLocation()
+  const isSelected = location.url.pathname.startsWith(href)
+  return (
+    <Link
+      href={href}
+      class={isSelected && styles.selected}>
+      {title}
+    </Link>
+  )
+})
 
 export default component$(() => {
   return (
@@ -10,8 +22,8 @@ export default component$(() => {
         <BetongromantikLogo class={styles.logo} />
       </Link>
       <nav class={styles.navigation}>
-        <Link href="/vykort">Vykort</Link>
-        <Link href="/instagram">Instagram</Link>
+        <NavLink href="/vykort" title="Vykort" />
+        <NavLink href="/instagram" title="Instagram" />
       </nav>
     </header>
   )
